dcff2496735aa9042afdd4e1b36d6a3688358ecb
Kazuki Tokunou <tokubd127@gmail.com>
2024-06-27 21:53:52 +0900

    Gitignore File作成、環境変数でDB接続作成

--- app/index.js
+++ app/index.js
@@ -23,10 +23,10 @@ const customers = [
 
 // データベースと接続
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "customer_info"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // データを取得できるようにしよう(GETメソッド)
